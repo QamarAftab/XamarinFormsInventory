f08463690b077813a75b3a339b12862c84267c4d
qamaraftab <aftabqamar@yahoo.com>
2019-02-16 17:45:33 +0500

    Add Radial Menu

--- InventoryManagementSystem/InventoryManagementSystem/Home/Dashboard.xaml.cs
+++ InventoryManagementSystem/InventoryManagementSystem/Home/Dashboard.xaml.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 using Xamarin.Forms;
+using Xamarin.Forms.RadialMenu.Enumerations;
+using Xamarin.Forms.RadialMenu.Models;
+using Xamarin.Forms.RadialMenu.ViewModels;
 using Xamarin.Forms.Xaml;
 
 namespace InventoryManagementSystem.Home
@@ -24,6 +28,33 @@ namespace InventoryManagementSystem.Home
                 });
             }
             Lst.ItemsSource = lstNumber;
+
+            var vm = new MainMenuViewModel();
+            BindingContext = vm;
+            vm.MenuItems = new ObservableCollection<RadialMenuItem>()
+            {
+                new RadialMenuItem()
+                {
+                    Source = "menu_paint.png",
+                    Title="Edit",
+                    WidthRequest = 38,
+                    HeightRequest = 38,
+                    VerticalOptions = LayoutOptions.Center,
+                    HorizontalOptions = LayoutOptions.Center,
+                    Location = Enumerations.RadialMenuLocation.N
+                }
+            };
+            vm.MenuItems.Add(new RadialMenuItem()
+            {
+                Title="Delete",
+                Source = "menu_lorry.png",
+                WidthRequest = 38,
+                HeightRequest = 38,
+                VerticalOptions = LayoutOptions.Center,
+                HorizontalOptions = LayoutOptions.Center,
+                Location = Enumerations.RadialMenuLocation.Ne
+            });
+
         }
 
         public class NumberClass
